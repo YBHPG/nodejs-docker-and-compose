8b3866fd320ab71cb09cb89b08a256be1b4b64cb
Vladislav <70971642+YBHPG@users.noreply.github.com>
2026-01-19 18:05:48 +0300

    Updated code

--- backend/src/wishlists/entities/wishlist.entity.ts
+++ backend/src/wishlists/entities/wishlist.entity.ts
@@ -1,40 +1,40 @@
 import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  ManyToMany,
-  ManyToOne,
-  JoinTable,
-  CreateDateColumn,
-  UpdateDateColumn,
-} from 'typeorm';
-import { Wish } from '../../wishes/entities/wish.entity';
-import { User } from '../../users/entities/user.entity';
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToMany,
+    ManyToOne,
+    JoinTable,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { Wish } from "../../wishes/entities/wish.entity";
+import { User } from "../../users/entities/user.entity";
 
 @Entity()
 export class Wishlist {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @CreateDateColumn()
-  createdAt: Date;
+    @CreateDateColumn()
+    createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
 
-  @Column({ length: 250 })
-  name: string;
+    @Column({ length: 250 })
+    name: string;
 
-  @Column({ length: 1500 })
-  description: string;
+    @Column({ nullable: true })
+    description: string;
 
-  @Column()
-  image: string;
+    @Column()
+    image: string;
 
-  @ManyToMany(() => Wish, (wish) => wish.wishlists)
-  @JoinTable()
-  items: Wish[];
+    @ManyToMany(() => Wish, (wish) => wish.wishlists)
+    @JoinTable()
+    items: Wish[];
 
-  @ManyToOne(() => User, (user) => user.wishlists)
-  owner: User;
+    @ManyToOne(() => User, (user) => user.wishlists)
+    owner: User;
 }
