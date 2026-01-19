cfd818f83fa05bba38a5277f85392f28d49b04e8
Vladislav <70971642+YBHPG@users.noreply.github.com>
2026-01-19 16:57:04 +0300

    Changed docker-compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,7 +12,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'database',
       port: 5432,
       username: 'postgresuser',
       password: 'passworduser',
